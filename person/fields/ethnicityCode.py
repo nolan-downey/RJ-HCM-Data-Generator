e0fbe8f8bfac1779d886af6196fc121f106a7d94
patricksoga <psoga@nd.edu>
2021-01-05 11:18:48 -0800

    ethnicityCode finished; put spike generation in its own module and imported it in person fields

--- person/fields/ethnicityCode.py
+++ person/fields/ethnicityCode.py
@@ -1,5 +1,3 @@
-import os, sys
-sys.path.insert(0, os.path.abspath(".."))
 from util.generateBiased import generateBiased
 
 ETHNICITY_CODES = [
@@ -14,16 +12,3 @@ ETHNICITY_CODES = [
 
 def newEthnicityCode(percentages):
   return generateBiased(ETHNICITY_CODES, percentages)
-
-def main():
-  count = 0
-  count_dict = {}
-  for i in range(100000):
-    eth = newEthnicityCode([0, 0.75, 29.25, 20, 25, 25])
-    count_dict[eth] = count_dict.get(eth, 0) + 1
-    if (eth): count += 1
-  print(count)
-  print(count_dict)
-
-if __name__ == "__main__":
-    main()
