a0375b3f7e082eb569c62b56c40efa933a5f71ca
Robert Reutiman <rreutima@nd.edu>
2021-01-05 07:29:37 -1000

    Initial address generation

--- address/address.py
+++ address/address.py
@@ -1,9 +1,62 @@
+from randomPercentages import generateRandomEntry
+import json
+
 #
 # @func   createAddress
 # @desc   Creates address, calls other functions to generate data
 # @param  None
 #
-def createAddress():
-  address = { "x": 1 }
+def createAddress(person):
+  address = {}
+
+  address["nameCode"] = "Robert Reutiman"
+  # address["nameCode"] = person.personName.firstName + person.personName.lastName
+  address["countryCode"] = "USA"
+
+  statesCities = json.load(open('statesCities.json', 'r'))
+
+  for key in list(statesCities.keys()):
+    
+
+  address["stateCode"] = newStateCode(list(statesCities.keys()))
+  address["cityName"], address["postalCode"] = newCityZip(statesCities[address["stateCode"]])
+  address["lineOne"] = newLineOne()
+  
+  # address["geoCoordinate"] = newGeoCoordinate()
+
+  person["address"] = address
 
   return address
+
+#
+# @func   newStateCode()
+# @desc   Creates new state code
+# @param  None
+#
+def newStateCode(states):
+  state = ""
+
+  state = generateRandomEntry(states, [2 for _ in range(50)])
+
+  return state
+
+#
+# @func   newCityZip()
+# @desc   Creates new city, zipcode
+# @param  None
+#
+def newCityZip(cityZips):
+
+  cityZip = generateRandomEntry(cityZips, [100 / len(cityZips) for _ in range(len(cityZips))])
+
+  return cityZip[0], cityZip[1]
+
+#
+# @func   newLineOne()
+# @desc   Creates new address line one
+# @param  None
+#
+def newLineOne():
+  lineOne = ""
+
+  return lineOne
