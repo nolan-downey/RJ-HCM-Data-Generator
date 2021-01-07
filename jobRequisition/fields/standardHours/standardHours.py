c0b3e921cd68b1bea42f2c729fa82039ac78b0e8
Tor Cox <torcox@Tors-Air.hsd1.mn.comcast.net>
2021-01-07 17:35:50 -0600

    standHours functions

--- jobRequisition/fields/standardHours/standardHours.py
+++ jobRequisition/fields/standardHours/standardHours.py
@@ -17,53 +17,52 @@ def newFullTimeEquivalency():
     # 0 = false 1 = true
     return fullTime
 
-def dailyHoursQuantity(standardHours["fullTimeEquivalency"]):
-    if standardHours["fullTimeEquivalency"] == 1:
+def dailyHoursQuantity(fTE):
+    if fTE == 1:
         x = np.random.normal(8,1,1)     
         dailyHoursQuantity = round(x[0])
         # 1 point with mean 8 and cov 2
-    elif standardHours["fullTimeEquivalency"] == 0:
+    elif fTE == 0:
         x = np.random.normal(4,1,1)
         dailyHoursQuantity = round(x[0])
     return dailyHoursQuantity
 
-
-def weeklyHoursQuantity(standardHours["fullTimeEquivalency"]):
-    if standardHours["fullTimeEquivalency"] == 1:
+def weeklyHoursQuantity(fTE):
+    if fTE == 1:
         x = np.random.normal(40,2,1)   
         weeklyHoursQuantity = round(x[0])  
         # 1 point with mean 8 and cov 2
-    elif standardHours["fullTimeEquivalency"] == 0:
+    elif fTE == 0:
         x = np.random.normal(20,2,1)
         weeklyHoursQuantity = round(x[0])
     return weeklyHoursQuantity
 
-def payPeriodHoursQuantity(standardHours["fullTimeEquivalency"]):
-    if standardHours["fullTimeEquivalency"] == 1:
+def payPeriodHoursQuantity(fTE):
+    if fTE == 1:
         x = np.random.normal(80,2,1)   
         payPeriodHoursQuantity = round(x[0])  
         # 1 point with mean 8 and cov 2
-    elif standardHours["fullTimeEquivalency"] == 0:
+    elif fTE == 0:
         x = np.random.normal(40,2,1)
         payPeriodHoursQuantity = round(x[0])
     return payPeriodHoursQuantity
 
-def monthlyHoursQuantity(standardHours["fullTimeEquivalency"]):
-    if standardHours["fullTimeEquivalency"] == 1:
+def monthlyHoursQuantity(fTE):
+    if fTE == 1:
         x = np.random.normal(165,5,1)   
         monthlyHoursQuantity = round(x[0])  
         # 1 point with mean 8 and cov 2
-    elif standardHours["fullTimeEquivalency"] == 0:
+    elif fTE == 0:
         x = np.random.normal(82,5,1)
         monthlyHoursQuantity = round(x[0])
     return monthlyHoursQuantity
 
-def annualHoursQuantity(standardHours["fullTimeEquivalency"]):
-    if standardHours["fullTimeEquivalency"] == 1:
+def annualHoursQuantity(fTE):
+    if fTE == 1:
         x = np.random.normal(2000,20,1)   
         annualHoursQuantity = round(x[0])  
         # 1 point with mean 8 and cov 2
-    elif standardHours["fullTimeEquivalency"] == 0:
+    elif fTE == 0:
         x = np.random.normal(1000,20,1)
         annualHoursQuantity = round(x[0])
     return annualHoursQuantity
