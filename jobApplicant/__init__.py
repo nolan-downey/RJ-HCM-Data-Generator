27005b255937dff2797b95b18976f9ca7be8e8a5
patricksoga <psoga@nd.edu>
2021-01-08 15:03:22 -0800

    finalized jobApplicant

---
+++ jobApplicant/__init__.py
@@ -0,0 +1,1 @@
+from .PreviousEmployer import createNewPreviousEmployer
--- jobApplicant/jobApplicant.py
+++
@@ -1,9 +0,0 @@
-#
-# @func   createJobApplicant
-# @desc   Creates job applicant, calls other functions to generate data
-# @param  None
-#
-def createJobApplicant():
-  applicant = { "x": 1 }
-
-  return applicant
