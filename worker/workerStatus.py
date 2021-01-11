b9b3414d7628bed69a87d6950b5d923919ce5b2f
Nolan Downey <ndowney@nd.edu>
2021-01-11 12:07:48 -0800

    Worker Status File Created

---
+++ worker/workerStatus.py
@@ -0,0 +1,10 @@
+import random
+from util.generateBiased import generateBiased
+
+#
+# @func createWorkerStatus
+# @desc gives each worker a work status
+# @param N/A
+#
+def workerStatus():
+    return
