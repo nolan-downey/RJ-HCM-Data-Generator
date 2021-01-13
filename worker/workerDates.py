1ade262969fea7e4d4e9cc49ec183c227aea60d5
Nolan Downey <ndowney@nd.edu>
2021-01-13 08:48:01 -0800

    First Draft

--- worker/workerDates.py
+++ worker/workerDates.py
@@ -1,19 +1,45 @@
 import datetime
 import random
-from util.generateBiased import generateBiased
+import sys
+sys.path.insert(0,"/mnt/c/Users/Nolan Downey/Desktop/RJReliance/RJ-HCM-Data-Generator/")
 from person import person
+
 
 #
 # @func createWorkerDates
-# @desc Creates essential dates for each worker
+# @desc Combines all essential dates into one object
 # @param None
 #
-#def createWorkerDates(percentages):
+def createWorkerDates():
+
+  workerDates = {}
+  workerDates["originalHireDate"] = createOriginalHireDate(person)
+  workerDates["rehireDate"] = createRehireDate(originalHireDate)
+
+  return workerDates
+
+#
+# @func createOriginalHireDate
+# @desc Creates originalHireDate for each worker
+# @param Person JSON Object, array of percentages
+#
+def createOriginalHireDate(person, percentages):
     
-startDt = person.birthDate + datetime.timedelta(weeks=1040)
-print(startDt)
-    #originalHireDate
-    #rehireDate
-    
-    
-    #return
+  startDt = person.birthDate + datetime.timedelta(weeks=1040)
+  endDt = datetime.datetime.now()
+  originalHireDate = datetime.datetime.fromordinal(random.randint(startDt, endDt))
+
+  return originalHireDate
+
+#
+# @func createRehireDate
+# @desc Creates rehire date for some of the workers
+# @param originalHireDate, array of percentages
+#
+def createRehireDate(originalHireDate):
+  
+  startDt = originalHireDate
+  endDt = datetime.datetime.now()
+  rehireDate = datetime.datetime.fromordinal(random.randint(startDt,endDt))
+
+  return randomDate
