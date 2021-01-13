89cccd948768cd29006ef31991609244249d28d4
Nolan Downey <ndowney@nd.edu>
2021-01-13 08:17:35 -0800

    Testing

--- worker/workerDates.py
+++ worker/workerDates.py
@@ -1,11 +1,19 @@
 import datetime
 import random
 from util.generateBiased import generateBiased
+from person import person
 
 #
 # @func createWorkerDates
 # @desc Creates essential dates for each worker
 # @param None
 #
-def createWorkerDates():
+#def createWorkerDates(percentages):
     
+startDt = person.birthDate + datetime.timedelta(weeks=1040)
+print(startDt)
+    #originalHireDate
+    #rehireDate
+    
+    
+    #return
