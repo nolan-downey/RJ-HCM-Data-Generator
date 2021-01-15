e51e893093c3905912146270602bc275bf09953c
patricksoga <psoga@nd.edu>
2021-01-15 10:09:47 -0800

    added front end and api beginnings

---
+++ hcm-front-end/src/App.js
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from 'react';
+import { APIURL, STATENAMES } from './literals';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [states, setStates] = useState();
+  const [dbs, setDbs] = useState();
+  
+  const getStateCode = (selection) => {
+    if (typeof(selection) !== 'string')
+      selection = selection.target.value;
+    axios.get(`${APIURL}/api/addresses?stateCode=${selection}`)
+      .then(response => {
+        setStates(response.data || 'No addresses');
+      })
+      .catch(error => {
+        setStates(error);
+      });
+  }
+
+  const handleChangeDB = (selection) => {
+    selection = selection.target.value;
+    axios.post(`${APIURL}/api/changeDB`, {"dbName": selection})
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  const getDBNames = useRef(() => {})
+  getDBNames.current = () => {
+    axios.get(`${APIURL}/api/getdbnames`)
+      .then(response => {
+        setDbs(response.data || 'No dbs');
+      })
+      .catch(error => {
+        setDbs(error);
+      });
+  };
+
+  useEffect(() => {
+    getStateCode(STATENAMES[0]);
+    getDBNames.current();
+  }, []);
+
+  return (
+    <div className="App"> 
+      <header className="App-header">
+        <label htmlFor="state" style={{display: 'block'}}>
+          Choose a database and state whose addresses you want to view
+        </label>
+        <select name="dbNames" onChange={handleChangeDB}>
+          {dbs && Array.isArray(dbs) ? 
+            dbs.length > 0 ? 
+              dbs.map((db, idx) => (
+                <option key={idx} value={db}>
+                  {db}
+                </option>))
+              : <> ... </>
+            : <> </>}
+        </select>
+        <select name="states" onChange={getStateCode}>
+          {STATENAMES.map((state, idx) => <option key={idx} value={state}>{state}</option>)}
+        </select>
+          {states && Array.isArray(states) ? 
+            states.length > 0 ? 
+              states.map((state, idx) => (
+                <span key={idx} style={{display: 'block'}}>
+                  Address {idx}: {state.cityName}, {state.stateCode} {state.countryCode}
+                </span>))
+              : <>...</>
+            : <>...</>}
+      </header>
+    </div>
+  )
+}
+export default App;
