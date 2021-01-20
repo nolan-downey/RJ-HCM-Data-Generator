3818141f83df8329b1eca5aa5dfce547ede22fe7
Robert Reutiman <rreutima@nd.edu>
2021-01-20 10:50:22 -0800

    Frame 1 - Select Table

--- hcm-front-end/src/App.js
+++ hcm-front-end/src/App.js
@@ -8,14 +8,15 @@ import Frame2 from "./frame2"
 function App() {
 
   const [frame, nextFrame] = useState(0);
+  const [table, setTable] = useState("");
 
   return (
     <Fragment>
       <Nav/>
        {/* Package each page content into different functions in other files */}
       {frame === 0 && <Frame0 next={() => nextFrame(1)}/>}
-      {frame === 1 && <Frame1 next={() => nextFrame(2)}/>}
-      {frame === 2 && <Frame2/>}
+      {frame === 1 && <Frame1 selectTable={table => setTable(table)} next={() => nextFrame(2)}/>}
+      {frame === 2 && <Frame2 table={table}/>}
     </Fragment>
   )
 }
